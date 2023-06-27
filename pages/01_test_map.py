2d79950743090eba544ad20b6ca0fb2215412c21
baamsoo <135572929+baamsoo@users.noreply.github.com>
2023-06-27 10:31:35 +0900

    Update 01_test_map.py

--- pages/01_test_map.py
+++ pages/01_test_map.py
@@ -11,6 +11,24 @@ st.title("Dot Map Visualization")
 
 data = common.get_sales()
 
+def rad2deg(radians):
+    degrees = radians * 180 / pi
+    return degrees
+
+def deg2rad(degrees):
+    radians = degrees * pi / 180
+    return radians
+
+def getDistanceBetweenPointsNew(latitude1, longitude1, latitude2, longitude2):
+    theta = longitude1 - longitude2
+    distance = 60 * 1.1515 * rad2deg(
+        arccos(
+            (sin(deg2rad(latitude1)) * sin(deg2rad(latitude2))) + 
+            (cos(deg2rad(latitude1)) * cos(deg2rad(latitude2)) * cos(deg2rad(theta)))
+        )
+    )
+    return round(distance * 1.609344, 2)
+
 def color_select(x):
     if x >= 45:
         return 'red'
