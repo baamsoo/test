b548ddb50fb00ee39d68b90d11b22dafd8cf40a0
baamsoo <135572929+baamsoo@users.noreply.github.com>
2023-06-27 12:07:05 +0900

    Update and rename 04_toal test.py to 04_total test.py

--- pages/04_total test.py
+++ pages/04_total test.py
@@ -5,12 +5,32 @@ import seaborn as sns
 from scipy.stats import linregress
 import pandas as pd
 
-
-
 common.page_config()
 st.title("PM10 Distribution by Top 5 Cities")
 
 data1 = common.get_sales()
+
+top_10 = pd.concat([PM10, PM2_5, SO2, NO2, O3, CO])
+
+plt.style.use('fivethirtyeight')
+fig,ax=plt.subplots(1,2,figsize=(15,8))
+clr = ("blue", "forestgreen", "gold", "red", "purple",'cadetblue','hotpink','orange','darksalmon','brown')
+top_10.Address.value_counts().sort_values(ascending=False)[:10].sort_values().plot(kind='barh',color=clr,ax=ax[0])
+ax[0].set_title("Top 10 Cities",size=20)
+ax[0].set_xlabel('Count',size=18)
+
+count=top_10['Address'].value_counts()
+groups=list(top_10['Address'].value_counts().index)[:10]
+counts=list(count[:10])
+counts.append(count.agg(sum)-count[:10].agg('sum'))
+groups.append('Other')
+type_dict=pd.DataFrame({"group":groups,"counts":counts})
+clr1=('brown','darksalmon','orange','hotpink','cadetblue','purple','red','gold','forestgreen','blue','plum')
+qx = type_dict.plot(kind='pie', y='counts', labels=groups,colors=clr1,autopct='%1.1f%%', pctdistance=0.9, radius=1.2,ax=ax[1])
+plt.legend(loc=0, bbox_to_anchor=(1.15,0.4))
+plt.subplots_adjust(wspace =0.5, hspace =0)
+plt.ioff()
+plt.ylabel('')
 
 fig, ax = plt.subplots(figsize=(10,6))
 sns.distplot(data1[data1.Address=='136, Hannam-daero, Yongsan-gu, Seoul, Republic of Korea'].PM10,color='maroon',hist=False,label='Hannam-daero')
