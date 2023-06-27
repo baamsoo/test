635d4661ee052a379da7fad6aa448ec4f4bd3aeb
baamsoo <135572929+baamsoo@users.noreply.github.com>
2023-06-27 10:31:19 +0900

    Update common.py

--- common.py
+++ common.py
@@ -4,23 +4,6 @@ import numpy as n
 from numpy import sin, cos, arccos, pi, round
 
 @st.cache_data
-def rad2deg(radians):
-    degrees = radians * 180 / pi
-    return degrees
-
-def deg2rad(degrees):
-    radians = degrees * pi / 180
-    return radians
-
-def getDistanceBetweenPointsNew(latitude1, longitude1, latitude2, longitude2):
-    theta = longitude1 - longitude2
-    distance = 60 * 1.1515 * rad2deg(
-        arccos(
-            (sin(deg2rad(latitude1)) * sin(deg2rad(latitude2))) + 
-            (cos(deg2rad(latitude1)) * cos(deg2rad(latitude2)) * cos(deg2rad(theta)))
-        )
-    )
-    return round(distance * 1.609344, 2)
 
 def get_sales():
     data = pd.read_csv("https://raw.githubusercontent.com/baamsoo/test/main/Measurement_summary.csv")
