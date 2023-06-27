4e034cf6a903167f4fd5d94a4368c89f7b74d9ff
baamsoo <135572929+baamsoo@users.noreply.github.com>
2023-06-27 09:53:46 +0900

    Update 01_test.py

--- pages/01_test.py
+++ pages/01_test.py
@@ -11,11 +11,6 @@ data = common.get_sales()
 
 seoul = folium.Map(location=[37.55138077230307, 126.98712254969668], zoom_start=12)
 
-# 위도 경도 DataFrame
-location = df.groupby('Station code')['PM10'].agg([np.mean])
-location['Latitude'] = df['Latitude'].unique()
-location['Longitude'] = df['Longitude'].unique()
-
 folium.Marker([37.4971850, 126.927595], icon=folium.Icon(popup='Dongjak-gu', color='red', icon='glyphicon glyphicon-home')).add_to(seoul)
 
 st_folium(seoul)
