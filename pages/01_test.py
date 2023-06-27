9a9b834686529e39b48316ad9bd25a2c6227dc78
baamsoo <135572929+baamsoo@users.noreply.github.com>
2023-06-27 09:43:00 +0900

    Update 01_test.py

--- pages/01_test.py
+++ pages/01_test.py
@@ -11,4 +11,23 @@ data = common.get_sales()
 
 seoul = folium.Map(location=[37.55138077230307, 126.98712254969668], zoom_start=12)
 
+# 위도 경도 DataFrame
+location = df.groupby('Station code')['PM10'].agg([np.mean])
+location['Latitude'] = df['Latitude'].unique()
+location['Longitude'] = df['Longitude'].unique()
+
+markers = 999
+loc_h = 0
+loc_v = 0
+
+for i in range(len(location)):
+    # 관측소
+    folium.Circle(location=[location.iloc[i,1], location.iloc[i,2]], radius = location.iloc[i, 0]*30, color=color_select(location.iloc[i,0]),fill_color='#ffffgg').add_to(seoul)
+    if getDistanceBetweenPointsNew(37.4971850, 126.927595, location.iloc[i,1], location.iloc[i,2]) < markers :
+        markers = getDistanceBetweenPointsNew(37.4971850, 126.927595, location.iloc[i,1], location.iloc[i,2])
+        loc_h, loc_v = location.iloc[i,1], location.iloc[i,2]
+      
+folium.Marker([37.4971850, 126.927595], icon=folium.Icon(popup='Dongjak-gu', color='red', icon='glyphicon glyphicon-home')).add_to(seoul)
+folium.Marker([loc_h, loc_v], icon=folium.Icon(popup='test', color='blue', icon='glyphicon glyphicon-home')).add_to(seoul)
+
 st_folium(seoul)
