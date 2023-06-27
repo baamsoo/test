f1662454efd3561ec8c2f3cb3ab0ebfee2a2505d
baamsoo <135572929+baamsoo@users.noreply.github.com>
2023-06-27 09:56:23 +0900

    Update 01_test.py

--- pages/01_test.py
+++ pages/01_test.py
@@ -11,6 +11,14 @@ data = common.get_sales()
 
 seoul = folium.Map(location=[37.55138077230307, 126.98712254969668], zoom_start=12)
 
+folium.CircleMarker([37.4971850, 126.927595],
+        radius=1,
+        color='red',
+        fill=True,
+        fill_color='red',
+        fill_opacity=0.6
+    ).add_to(seoul)
+
 folium.Marker([37.4971850, 126.927595], icon=folium.Icon(popup='Dongjak-gu', color='red', icon='glyphicon glyphicon-home')).add_to(seoul)
 
 st_folium(seoul)
